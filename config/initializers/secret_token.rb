732b9abcd18e7c3077dc01bf43a45c4991f3cfae
nono <5irehands@gmail.com>
2013-12-04 01:47:17 +0800

    first commit, basic blog CRUD and home page changed, this project refers to RailsGuides.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9973e588f133f29ce95885092050e9c7f51fb840f83c0e71cfb170b58adb243a64cd15e8c226ad3bd859cd18fbf53a248a186c2d1d5263230d05de26f04a4bbb'
